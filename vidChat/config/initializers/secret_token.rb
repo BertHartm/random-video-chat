8210bffda5ab7f2931e7b3b7b02bfa4a371e6736
Gilbert Hartmann <BertHartm@gmail.com>
2011-02-24 00:04:43 -0500

    adding a bare rails repo (used: -O -J -T to get rid of activerecord, prototype, and tests, as this will be a VERY small install)

---
+++ vidChat/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VidChat::Application.config.secret_token = '6745c017940f129a175c606b10338740eb82c8c1546862245d4f7e76fe98c2e656f94ea53b6f47799bb26caf6e0a3333e3aa943e50180898a736a2b56a259caf'
